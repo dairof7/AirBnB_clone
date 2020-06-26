f1247e8995b016e76152209e496691f360e63287
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-26 22:24:01 +0000

    Update basemodel and create file_storage class

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
+                    setattr(self, key, val)
                     continue
                 if key != "__class__":
                     setattr(self, key, value)
@@ -17,12 +17,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return "[{:s}] ({:s}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         new = dict(self.__dict__)
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module that contain file storage class"""
+from models.base_model import BaseModel
+import os.path
+import json
+
+class FileStorage():
+    """FileStorage    """
+    
+    __file_path = "file.json" 
+    __objects = {}
+
+    def all(self):
+        """all        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """new 
+        Args:
+            obj ([type]): [description]
+        """
+        key = type(obj).__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """save     """
+        for key, obj in FileStorage.__objects.items():
+            FileStorage.__objects[key] = obj.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as json_f:
+            json_f.write(json.dumps(FileStorage.__objects))
+
+    def reload(self):
+        """reload        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as js_f:
+                for key, obj in json.loads(js_f.read()).items():
+                    obj = eval(obj['__class__'])(**obj)
+                    FileStorage.__objects[key] = obj
