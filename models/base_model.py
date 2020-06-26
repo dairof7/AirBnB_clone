165c434b54af132991303cc3af8e9abd890207e6
dairof7 <dairof7@gmail.com>
2020-06-26 11:45:23 -0500

    update init

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,19 @@ from datetime import datetime
 import models
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{:s}] ({:s}) {}".format(type(self).__name__, self.id, self.__dict__)
